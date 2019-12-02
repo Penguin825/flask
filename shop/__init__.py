36352c395aa48108c184da7ed1c7b4ef2e721d9e
Penguin825 <44059932+Penguin825@users.noreply.github.com>
2019-12-02 15:31:28 +0000

    Update __init__.py

--- shop/__init__.py
+++ shop/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '76cc7027fa79aae3ed9b6153489c49958410b5c8496a7419'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysqsl+pymysql://C1805223:P3nGaminguin@csmysql.cs.cf.ac.uk:3306/C1805223'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
 
