263b52576f3ec04e48fdd53d0e7d0d75e1b8532e
Penguin825 <44059932+Penguin825@users.noreply.github.com>
2019-12-02 15:35:45 +0000

    Update __init__.py

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '76cc7027fa79aae3ed9b6153489c49958410b5c8496a7419'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
